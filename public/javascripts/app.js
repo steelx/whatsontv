8651f075a72208e7b14527b149370439167c6010
Ajinkya Borade <ajinkyab1987@gmail.com>
2014-06-10 15:31:54 +0530

    Updated appJs to fit in Uglify and enabled html5mode.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,6 @@
 angular.module('WhatsOnTV', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-  .config(function() {
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true);
 
-  });
+
+  }]);
