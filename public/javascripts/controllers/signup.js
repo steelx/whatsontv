4da41374e591562ac9834defadcd3d8953a915e5
Ajinkya Borade <ajinkyab1987@gmail.com>
2014-06-20 16:21:36 +0530

    Few more controllers and views. repeatPassword.js directive. signup.js controller. subscription.js service. login and signup.html view.

---
+++ public/javascripts/controllers/signup.js
@@ -0,0 +1,8 @@
+Whatsontv.controller('SignupCtrl', ['$scope', 'Auth', function($scope, Auth) {
+    $scope.signup = function() {
+      Auth.signup({
+        email: $scope.email,
+        password: $scope.password
+      });
+    };
+  }]);
---
+++ public/javascripts/services/subscription.js
@@ -0,0 +1,10 @@
+Whatsontv.factory('Subscription', ['$http', function($http) {
+    return {
+      subscribe: function(show, user) {
+        return $http.post('/api/subscribe', { showId: show._id });
+      },
+      unsubscribe: function(show, user) {
+        return $http.post('/api/unsubscribe', { showId: show._id });
+      }
+    };
+  }]);
