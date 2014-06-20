5cf4fd7bb89de3ed8541f447555290892ce20e76
Ajinkya Borade <ajinkyab1987@gmail.com>
2014-06-20 16:07:15 +0530

    Controller fromNow.js

---
+++ public/filters/fromNow.js
@@ -0,0 +1,5 @@
+Whatsontv.filter('fromNow', function() {
+    return function(date) {
+      return moment(date).fromNow();
+    }
+});
--- public/javascripts/controllers/add.js
+++ public/javascripts/controllers/add.js
@@ -1,5 +1,4 @@
-angular.module('MyApp')
-  .controller('AddCtrl', ['$scope', '$alert', 'Show', function($scope, $alert, Show) {
+Whatsontv.controller('AddCtrl', ['$scope', '$alert', 'Show', function($scope, $alert, Show) {
     $scope.addShow = function() {
       Show.save({ showName: $scope.showName },
         function() {
