474194262b4d1ec0aa5dc08b4c9ee74b33329d0c
Ajinkya Borade <ajinkyab1987@gmail.com>
2014-06-20 16:25:16 +0530

    controller login.js. service auth.js

---
+++ public/javascripts/controllers/login.js
@@ -0,0 +1,8 @@
+Whatsontv.controller('LoginCtrl', ['$scope', 'Auth', function($scope, Auth) {
+    $scope.login = function() {
+      Auth.login({
+        email: $scope.email,
+        password: $scope.password
+      });
+    };
+  }]);
